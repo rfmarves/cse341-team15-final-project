81a6086b1809190eef18e7f01a987a35a1a4975a
rfmarves <rfmarves@gmail.com>
2025-07-31 17:45:17 -0600

    initialized repository

---
+++ swagger.js
@@ -0,0 +1,21 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Event Ticketing - CSE341 Final Project"',
+    description: 'API for ticking data as project2 for CSE 341'
+  },
+  host: '',
+  schemes: ['http', 'https']
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./routes/index.js'];
+
+// generate swagger.json
+swaggerAutogen(outputFile, endpointsFiles, doc);
+
+// Run server after it gets generated
+// swaggerAutogen(outputFile, endpointsFiles, doc).then(async () => {
+//   await import('./index.js');
+// });
